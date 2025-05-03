84b779656afa7797bb4fd924dc580f40ec0fbbd3
franciscof <franciscof@cetsp.com.br>
2025-05-03 00:34:33 -0300

    Implementa a aula de Conceitos e testes

---
+++ ContainRs.Testes/EmailCtor.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using ContainRs.WebApp.Models;
+
+namespace ContainRs.Testes
+{
+    public class EmailCtor
+    {
+        [Fact]
+        public void Deve_Lancar_ArgumentException_Quando_Valor_Invalido()
+        {
+            //arrange
+            string emailInvalido = "valor qualquer";
+
+            //act & assert
+            Assert.Throws<ArgumentException>(() => new Email(emailInvalido));
+
+            //assert
+        }
+    }
+}
---
+++ ContainRs.WebApp/Models/Email.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ContainRs.WebApp.Models;
+
+public class Email
+{
+    private static readonly Regex EmailRegex = new(
+        @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+        RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+    public Email(string value)
+    {
+        // validação
+        if (!EmailRegex.IsMatch(value))
+        {
+            throw new ArgumentException("E-mail inválido.");
+        }
+
+        Value = value;
+    }
+
+    public string Value { get; }
+}
