4f18926ff1af350877685034e4e7293fb880d638
franciscof <franciscof@cetsp.com.br>
2025-05-06 00:10:03 -0300

    Implementa a aula Registrando Clientes com um EdnPoint

--- ContainRs.WebApp/Controllers/RegistroController.cs
+++ ContainRs.WebApp/Controllers/RegistroController.cs
@@ -1,5 +1,7 @@
-﻿using ContainRs.WebApp.Data;
+﻿using Azure.Core;
+using ContainRs.WebApp.Data;
 using ContainRs.WebApp.Models;
+using ContainRs.WebApp.UseCases;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ContainRs.WebApp.Controllers;
@@ -34,19 +36,11 @@ public class RegistroController : Controller
             return View("Index", form);
         }
 
-        var cliente = new Cliente(form.Nome, new Email(form.Email), form.CPF)
-        {
-            Celular = form.Celular,
-            CEP = form.CEP,
-            Rua = form.Rua,
-            Numero = form.Numero,
-            Complemento = form.Complemento,
-            Bairro = form.Bairro,
-            Municipio = form.Municipio,
-            Estado = form.Estado
-        };
-        context.Clientes.Add(cliente);
-        await context.SaveChangesAsync();
+        var useCase = new RegistrarCliente(context, form.Nome, new Email(form.Email), form.CPF, form.Celular,
+        form.CEP, form.Rua, form.Numero, form.Complemento, form.Bairro, form.Municipio,
+        form.Estado);
+
+        await useCase.ExecutarAsync();
 
         return RedirectToAction("Sucesso");
     }
